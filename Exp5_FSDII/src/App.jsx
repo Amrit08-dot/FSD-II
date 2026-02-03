3dd69bce7af8b43af6814ce344eb07483a9afc97
Amrit08-dot <amrit.nijj814@gmail.com>
2026-02-03 14:43:10 +0530

    .

--- Exp5_FSDII/src/App.jsx
+++ Exp5_FSDII/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { Suspense } from 'react';
+import SinglePageApp from './components/Spa.jsx';
 
 const Dashboard = React.lazy(() => import('./components/Dashboard'));
 
 function App() {
   return (
-    <Suspense fallback={<h3>Loading Dashboard...</h3>}>
-      <Dashboard />
-    </Suspense>
+    <>
+      <Suspense fallback={<h3>Loading Dashboard...</h3>}>
+        <Dashboard />
+        <SinglePageApp />
+      </Suspense>
+
+      
+    </>
   );
 }
 
